f0eef2a578236c42beae16371ec6461a6956665d
praniil <pparajuli15@gmail.com>
2023-07-20 06:48:39 +0545

    feat: added router

--- router/router.go
+++ router/router.go
@@ -1,5 +1,15 @@
 package router
 
-func Router(){
-	
+import (
+	"go-todoapp/handler"
+
+	"github.com/gorilla/mux"
+)
+
+//Router is exported and used in main.go
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/newtodos", handler.CreateTodolist).Methods("POST", "OPTIONS")
+	return router
 }
